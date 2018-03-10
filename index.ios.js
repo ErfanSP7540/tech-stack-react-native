3437b644c89619fda7c6f5e81832ee4ee98f66be
erfanSP <erfansp@erfanSPs-Mac.local>
2018-03-10 21:27:39 +0330

    init

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+
+
+import React, { Component } from 'react';
+import {AppRegistry,Text} from 'react-native';
+
+export default class tech_stack extends Component {
+  render() {
+    return (
+        <Text >
+          tech_stack
+        </Text>
+    );
+  }
+}
+
+
+AppRegistry.registerComponent('tech_stack', () => tech_stack);
