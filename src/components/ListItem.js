1484312a0831f912ecec0b4a36ad11813992d7a6
erfanSP <erfansp@erfanSPs-Mac.local>
2018-03-11 17:13:12 +0330

    selectionReducer

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import {Text} from 'react-native'
+import {Text , View , TouchableWithoutFeedback} from 'react-native'
 import {CardSection} from './common'
 
 import {connect} from 'react-redux'
@@ -8,13 +8,20 @@ import * as actions from '../reducers/actions'
 
 class ListItem extends  Component{
     render(){
+        const {title,id}=this.props.library;
 
         console.log(this.props);
         
         return (
-            <CardSection>
-                <Text style={styles.titleStyle}>{this.props.library.title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback 
+                onPress={()=>this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={styles.titleStyle}>{title}</Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
