165d3c853e62ce7cf89f0bf053e798586db74fda
erfanSP <erfansp@erfanSPs-Mac.local>
2018-03-11 16:32:57 +0330

    actions mapReducer2prop

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,8 +2,15 @@ import React,{Component} from 'react'
 import {Text} from 'react-native'
 import {CardSection} from './common'
 
+import {connect} from 'react-redux'
+import * as actions from '../reducers/actions'
+
+
 class ListItem extends  Component{
     render(){
+
+        console.log(this.props);
+        
         return (
             <CardSection>
                 <Text style={styles.titleStyle}>{this.props.library.title}</Text>
@@ -18,4 +25,9 @@ const styles = {
         paddingLeft:15,
     },
 }
-export default ListItem
+
+const mapState2props=(state)=>{
+    return { selectedID:state.selectedLibraryId }
+}
+
+export default connect(mapState2props,actions)(ListItem)
